67e7f99af6bbfc19bb0d6b0002608dc97de30f89
shokhzodbek <66237287+shokhzodbek@users.noreply.github.com>
2023-09-30 10:55:29 +0500

    python

---
+++ func.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    print(a+b)
+    
+def ayir(a,b):
+    print(a-b)
+    
+def mult(a,b):
+    print(a*b)
+
+def div(a,b):
+    print(a/b)
---
+++ main.py
@@ -0,0 +1,3 @@
+import random
+a = random.randint(1,100)
+print(a)
